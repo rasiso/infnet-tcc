d7255cb5f124ea9861a5799eb2917825a9f4142f
Marcos <Marcos@pcmarcos>
2018-08-12 19:09:01 -0300

    roolback

--- tcc/src/main/java/br/edu/infnet/model/Usuario.java
+++ tcc/src/main/java/br/edu/infnet/model/Usuario.java
@@ -1,57 +1,27 @@
 package br.edu.infnet.model;
 
 import java.io.Serializable;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
 
-import org.hibernate.validator.constraints.Length;
-import org.springframework.context.annotation.Role;
-import org.springframework.data.annotation.Transient;
-
+import org.hibernate.annotations.NaturalId;
 
 @Entity
-@Table(name = "usuario")
 public class Usuario implements Serializable {
+
 	private static final long serialVersionUID = -4561964530177975659L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id")
+	@GeneratedValue
 	private Long id;
-	
-	@Column(name = "email")
-	private String email;
-	
-	@Column(name = "password")
-	@Length(min = 5, message = "*Sua senha deve conter 6 caractere no minimo")
-	
-	@Transient
-	private String password;
-	
-	@Column(name = "nome")
-	private String nome;
-	
-	@Column(name = "active")
-	private int active;
-	
-	@ManyToMany(cascade = CascadeType.ALL )
-	@JoinTable(name = "usuario_role", joinColumns = @JoinColumn
-	          (name = "usuario_id" ,referencedColumnName = "id"), 
-	          inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-	private Set<Role> roles;
 
-	public Usuario() {};
-	
+	@NaturalId
+	private String login;
+
+	private String senha;
+
 	public Long getId() {
 		return id;
 	}
@@ -60,56 +30,29 @@ public class Usuario implements Serializable {
 		this.id = id;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getLogin() {
+		return login;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setLogin(String login) {
+		this.login = login;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getSenha() {
+		return senha;
 	}
 
-	public void setName(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public int getActive() {
-		return active;
-	}
-
-	public void setActive(int active) {
-		this.active = active;
-	}
-
-	public Set<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + active;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((roles == null) ? 0 : roles.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
 		return result;
 	}
 
@@ -122,38 +65,22 @@ public class Usuario implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Usuario other = (Usuario) obj;
-		if (active != other.active)
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (nome == null) {
-			if (other.nome != null)
+		if (login == null) {
+			if (other.login != null)
 				return false;
-		} else if (!nome.equals(other.nome))
+		} else if (!login.equals(other.login))
 			return false;
-		if (password == null) {
-			if (other.password != null)
+		if (senha == null) {
+			if (other.senha != null)
 				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		if (roles == null) {
-			if (other.roles != null)
-				return false;
-		} else if (!roles.equals(other.roles))
+		} else if (!senha.equals(other.senha))
 			return false;
 		return true;
 	}
 
-	
-	
-	
 }
-
