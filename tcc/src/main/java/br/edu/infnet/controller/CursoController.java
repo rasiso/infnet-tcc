36e69c988c4d5056ca80f0ffa15b47877d3b8482
https://github.com/infnetgit/infnet.git <rasiso@hotmail.com>
2018-08-12 16:56:47 -0300

    Tela e controller de curso usando requests GET, POST, PUT e DELETE

--- tcc/src/main/java/br/edu/infnet/controller/CursoController.java
+++ tcc/src/main/java/br/edu/infnet/controller/CursoController.java
@@ -2,10 +2,12 @@ package br.edu.infnet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,25 +23,36 @@ public class CursoController {
 	
 	@GetMapping
 	public ModelAndView listar() {
-		
 		ModelAndView mav = new ModelAndView("cursoMain");
 		mav.addObject(new Curso());
-		mav.addObject("cursos", this.cursoRepository.findAll()).addObject(new Curso());
+		mav.addObject("cursos", this.cursoRepository.findAll());
 		return mav;
-		
+	}
+	
+	@GetMapping("/{id}")
+	public ModelAndView getById(@PathVariable long id, ModelMap model) {
+		ModelAndView mav = new ModelAndView("cursoMain");
+		mav.addObject("curso", this.cursoRepository.findById(id));
+		mav.addObject("cursos", this.cursoRepository.findAll());
+		mav.addObject("method", "PUT");
+		return mav;
 	}
 		
-	
 	@PostMapping
 	public String salvar(Curso curso) {
 		this.cursoRepository.save(curso);
 		return "redirect:/curso";
 	}
 	
-	@DeleteMapping("/{id}")
+	@GetMapping("/delete/{id}")
 	public String deletar(@PathVariable long id) {
 		this.cursoRepository.deleteById(id);
 		return "redirect:/curso";
+	}
+	
+	@PutMapping
+	public String editar(Curso curso) {
+		return this.salvar(curso);
 	}
 
 }
