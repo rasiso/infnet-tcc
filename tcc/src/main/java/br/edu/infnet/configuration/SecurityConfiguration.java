bd326e06f59359bca2b475f987d0c606b340f931
Marcos <Marcos@pcmarcos>
2018-08-15 22:12:55 -0300

    config security

--- tcc/src/main/java/br/edu/infnet/configuration/SecurityConfiguration.java
+++ tcc/src/main/java/br/edu/infnet/configuration/SecurityConfiguration.java
@@ -45,6 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		
 		http.
 			authorizeRequests()
+	            .anyRequest().authenticated()
 				.antMatchers("/").permitAll()
 				.antMatchers("/login").permitAll()
 				
