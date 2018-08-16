db25ba9f0625e02c4ae5f6c52e6cfcf94c659187
Marcos <Marcos@pcmarcos>
2018-08-16 20:37:32 -0300

    ctrl usuario turma

--- tcc/src/main/java/br/edu/infnet/controller/LoginController.java
+++ tcc/src/main/java/br/edu/infnet/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -79,8 +80,13 @@ public class LoginController {
 		return mav;
 	
 	}*/
-	
-	
+
+	@RequestMapping(value={"/", "/turma"}, method = RequestMethod.GET)
+	public ModelAndView turma(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("turma");
+		return modelAndView;
+	}
 	
 
 }
