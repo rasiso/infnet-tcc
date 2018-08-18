bc845017dc29eb8e2a46945fa2d1b16153c1ac13
Marcos <Marcos@pcmarcos>
2018-08-18 06:52:53 -0300

    acerto

--- tcc/src/main/java/br/edu/infnet/model/Role.java
+++ tcc/src/main/java/br/edu/infnet/model/Role.java
@@ -17,24 +17,26 @@ public class Role implements Serializable {
 
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="id")
-	private Long id;
+	@Column(name="role_id")
+	private Long role_id;
 	
 	@Column(name="role")
 	private String role;
 	
-	public Role() {};
+	public Role(){};
     
 	public Role(String role) {
         this.role = role;
     }
-	
-	public Long getId() {
-		return id;
+
+	public Long getRole_id() {
+		return role_id;
 	}
-	public void setId(Long id) {
-		this.id = id;
+
+	public void setRole_id(Long role_id) {
+		this.role_id = role_id;
 	}
+
 	public void setrole(String role) {
 		this.role=role;
 	}
@@ -49,7 +51,7 @@ public class Role implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((role_id == null) ? 0 : role_id.hashCode());
 		result = prime * result + ((role == null) ? 0 : role.hashCode());
 		return result;
 	}
@@ -62,10 +64,10 @@ public class Role implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Role other = (Role) obj;
-		if (id == null) {
-			if (other.id != null)
+		if (role_id == null) {
+			if (other.role_id != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!role_id.equals(other.role_id))
 			return false;
 		if (role == null) {
 			if (other.role != null)
