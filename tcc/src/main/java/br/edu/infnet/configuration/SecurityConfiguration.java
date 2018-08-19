e54d7adc24997c7f66f5ab9b82cf9e9b21f2a92b
Marcos <Marcos@pcmarcos>
2018-08-19 09:28:57 -0300

    acerto

--- tcc/src/main/java/br/edu/infnet/configuration/SecurityConfiguration.java
+++ tcc/src/main/java/br/edu/infnet/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +12,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +30,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Value("${spring.queries.roles-query}")
 	private String rolesQuery;
 
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth)
 			throws Exception {
@@ -37,7 +46,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.usersByUsernameQuery(usersQuery)
 				.authoritiesByUsernameQuery(rolesQuery)
 				.dataSource(dataSource)
-				.passwordEncoder(bCryptPasswordEncoder);
+				.passwordEncoder(passwordEncoder());
 		System.out.println(rolesQuery);
 		
 	}
@@ -46,14 +55,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http
-        .authorizeRequests()
+        .authorizeRequests().anyRequest().authenticated()
             .antMatchers("/login").permitAll()
             .antMatchers("/").permitAll()
             .antMatchers("/turma").hasRole("ADMIN")
             .anyRequest().authenticated()
             .and()
             .formLogin().loginPage("/login").usernameParameter("email")
-            .passwordParameter("password1")
+            .passwordParameter("password")
             .defaultSuccessUrl("/main",true)
             .permitAll()
             .and()
