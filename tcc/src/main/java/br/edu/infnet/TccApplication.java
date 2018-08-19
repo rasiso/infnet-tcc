611b233873c1ad65e9e729e6568acc833ef77c4d
Marcos <Marcos@pcmarcos>
2018-08-18 21:19:34 -0300

    COMPONENTSCAN

--- tcc/src/main/java/br/edu/infnet/TccApplication.java
+++ tcc/src/main/java/br/edu/infnet/TccApplication.java
@@ -3,8 +3,10 @@ package br.edu.infnet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@ComponentScan({"br.edu.infnet.controller","br.edu.infnet.service"})
 @SpringBootApplication
 public class TccApplication {
 
