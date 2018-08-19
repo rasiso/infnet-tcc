ca695a6c5e283fd2bc3d0d8d3bb6556fef730d3b
Marcos <Marcos@pcmarcos>
2018-08-18 21:37:25 -0300

    remove main

--- tcc/src/main/java/br/edu/infnet/configuration/SecurityConfiguration.java
+++ tcc/src/main/java/br/edu/infnet/configuration/SecurityConfiguration.java
@@ -44,8 +44,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		  http.authorizeRequests()
-	          .antMatchers("/main","/").permitAll(); 
+		  //http.authorizeRequests()
+	      //    .antMatchers("/main","/").permitAll(); 
 		  
 		  http.authorizeRequests().antMatchers("/turma").hasRole("ADMIN");
 
