d8810aebd40da04ff0ee29ece1598003f3d34b8a
Marcos <Marcos@pcmarcos>
2018-08-26 19:49:20 -0300

    correcao security

--- tcc/src/main/java/br/edu/infnet/configuration/SecurityConfiguration.java
+++ tcc/src/main/java/br/edu/infnet/configuration/SecurityConfiguration.java
@@ -5,17 +5,14 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -50,13 +47,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.
-		authorizeRequests()
-			.antMatchers("/").permitAll()
+		
+		http.csrf().disable()
+		    .authorizeRequests()
+		    .antMatchers("/").permitAll()
 			.antMatchers("/login").permitAll()
-			.antMatchers("/main").permitAll()
+			.antMatchers("/main/**").permitAll()
 			.antMatchers("/turma").hasAuthority("ADMIN").anyRequest()
-			.authenticated().and().csrf().disable().formLogin()
+			.authenticated().and().formLogin()
 			.loginPage("/login").failureUrl("/login?error=true")
 			.defaultSuccessUrl("/main")
 			.usernameParameter("email")
@@ -65,13 +63,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 			.logoutSuccessUrl("/").and().exceptionHandling()
 			.accessDeniedPage("/403");
+		
+		http.csrf().disable();
+		
 	}
 	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
+	
+    @Override
+    public void configure(WebSecurity web) throws Exception {
 	    web
 	       .ignoring()
 	       .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
-	}
+    };
 
 }
