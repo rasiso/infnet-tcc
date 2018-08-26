f7fa69b91e5d3b89bb6f55fc6b32f941153dec5e
Marcos <Marcos@pcmarcos>
2018-08-26 19:40:35 -0300

    acerto

--- tcc/src/main/java/br/edu/infnet/TccApplication.java
+++ tcc/src/main/java/br/edu/infnet/TccApplication.java
@@ -2,6 +2,7 @@ package br.edu.infnet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 //import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
